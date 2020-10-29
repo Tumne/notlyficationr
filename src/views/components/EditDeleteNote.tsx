fddb03bb303ad203ef234b42a4c130bf31095714
Tumne <freshlygrazed@gmail.com>
2020-10-29 05:59:41 -0400

    minor refactor edit notes

--- src/views/components/EditDeleteNote.tsx
+++ src/views/components/EditDeleteNote.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react';
 import { TYPE } from '../../state/constants';
 import { NoteContext } from '../../state/noteContext';
-import { Button, Input } from '../common';
+import { Button, Form, Input } from '../common';
 
 interface Props {
   id: string;
@@ -13,30 +13,31 @@ export const EditDeleteNote: React.FC<Props> = ({ id, text }) => {
   const [toggle, setToggle] = useState(false);
   const [newText, setNewText] = useState(text);
 
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    if (toggle) {
+      dispatch({ type: TYPE.EDIT_NOTE, payload: { id, text: newText } });
+    }
+    setToggle(!toggle);
+  };
   return (
-    <li key={id}>
+    <Form onSubmit={handleSubmit}>
       {toggle ? (
         <Input value={newText} onChange={(e) => setNewText(e.target.value)} />
       ) : (
         text
       )}
-      <Button
-        onClick={() => {
-          if (toggle) {
-            dispatch({ type: TYPE.EDIT_NOTE, payload: { id, text: newText } });
-          }
-          setToggle(!toggle);
-        }}
-      >
+      <Button type="button" onClick={handleSubmit}>
         {toggle ? 'Save' : 'Edit'}
       </Button>
       <Button
+        type="button"
         onClick={() => {
           dispatch({ type: TYPE.DELETE_NOTE, payload: id });
         }}
       >
         x
       </Button>
-    </li>
+    </Form>
   );
 };
